6363639b531c2e1ebab6ba54731d27c6d580d276
ramin <@rraahmad>
2024-08-08 21:31:18 -0400

    the last codes of to night

--- src/testing.java
+++ src/testing.java
@@ -5,6 +5,25 @@ public class testing {
 
         System.out.println("hey this is Ahmad Ramin's work for class work ");
 
+        int num= 777;
+                int num1= 333;
+
+        System.out.println(num+num1);
+
+        System.out.println("this is my new work with micheal corleon ");
+        int nu3 = 12348;
+        int nu1 = 12348;
+        System.out.println(nu3-nu1);
+
+        System.out.println("ihuweiundka asddjfaodsjfoisdjfioaodjaidsjosjsdfasdfsdfisfahsdsa dsasdhfushfashfisdhafiuadhfsdhfisdhsdifhsdufhsdihfs" +
+                "ssduhfsdifhsdifhsdufhsdihfidhfidsishfisduhfi");
+
+
+        int num2 = 1500;
+                int num3= 2500;
+                        int num4 = 2400;
+                                int avg = num2+ num3+num4;
+        System.out.println(avg);
 
 
 
